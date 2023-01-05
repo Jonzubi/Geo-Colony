d593b28127cff174436230662da50a5e3a021072
Jon Zubizarreta <jonzubi97@gmail.com>
2023-01-05 18:13:06 +0100

    Separo managers

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager _gameManager;
+    public static SpawnManager _spawnManager;
     
     public static GameManager GetGameManager()
     {
         if (_gameManager == null)
+        {
             _gameManager = new GameManager();
+            _spawnManager = new SpawnManager();
+        }
 
         return _gameManager;
     }
---
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public List<GeoChild> _geoChilds;
+    public List<Food> _foods;
+
+    public SpawnManager()
+    {
+        _geoChilds = new List<GeoChild>();
+        _foods = new List<Food>();
+    }
+
+    public List<GeoChild> GeoChildren
+    {
+        get
+        {
+            return _geoChilds;
+        }
+    }
+
+    public List<Food> Foods
+    {
+        get
+        {
+            return _foods;
+        }
+    }
+}
