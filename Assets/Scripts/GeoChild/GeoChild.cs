4072811dbcd72ce84ff4ab7dac15ef5904a24093
Jon Zubizarreta <jonzubi97@gmail.com>
2023-01-06 23:58:13 +0100

    GeoChild tiene Id y FoodEaten

--- Assets/Scripts/GeoChild/GeoChild.cs
+++ Assets/Scripts/GeoChild/GeoChild.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class GeoChild : MonoBehaviour
 {
+    public int Id
+    {
+        get; set;
+    }
     float _moveSpeed = 30f;
+    
     public string Name
     {
         get; set;
+    }
+
+    public int FoodEaten
+    {
+        get; set;
+    }
+
+    GameManager _gameManager;
+
+    void Awake()
+    {
+        _gameManager = FindObjectOfType<GameManager>();    
     }
 
     public float GetMoveSpeed()
@@ -17,6 +34,10 @@ public class GeoChild : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log("Collision");    
+        if (other.gameObject.tag == "Food")
+        {
+            FoodEaten++;
+            _gameManager.GetSpawnManager().DestroyChild(other.gameObject.GetComponent<Food>().Id);
+        }
     }
 }
