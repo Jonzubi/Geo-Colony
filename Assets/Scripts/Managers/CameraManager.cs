4973c2b778c5c6fd5d1964f769d3189c7889e5d4
Jon Zubizarreta <jonzubi97@gmail.com>
2023-01-10 21:48:41 +0100

    Mejoro el zoom

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -1,13 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class CameraManager : MonoBehaviour
 {
+    [SerializeField] CinemachineVirtualCamera _cinemachineVirtualCamera;
+    [SerializeField] float zoomScale = 1f;
+    [SerializeField] float moveScale = 1f;
     GameManager _gameManager;
+
+    CinemachineComponentBase _componentBase;
 
     void Awake()
     {
         _gameManager = FindObjectOfType<GameManager>();    
+    }
+
+    void Update()
+    {
+        float mouseWheel = Input.mouseScrollDelta.y;
+        if (mouseWheel != 0)
+        {
+            float oldSize = _cinemachineVirtualCamera.m_Lens.OrthographicSize;
+            float newSize = oldSize - Input.mouseScrollDelta.y * zoomScale;
+
+            if (newSize < 5)
+                newSize = 5;            
+            _cinemachineVirtualCamera.m_Lens.OrthographicSize = Mathf.SmoothStep(oldSize, newSize, 1f);
+        }
     }
 }
