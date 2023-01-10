137c2427b3e2e80cad9aadf23ed270af73af47bb
Jon Zubizarreta <jonzubi97@gmail.com>
2023-01-10 20:54:03 +0100

    INCREASE_PER_GEOCHILDen ConfigurationManager

--- Assets/Scripts/Managers/ConfigurationManager.cs
+++ Assets/Scripts/Managers/ConfigurationManager.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class ConfigurationManager : MonoBehaviour
 {
     float _spawnFoodTime = 5;
+    const int INCREASE_PER_GEOCHILD = 1;
 
     public float SpawnFoodTime
     {
         get { return _spawnFoodTime; }
         set { _spawnFoodTime = value; }
+    }
+
+    public int IncreasePerGeoChild
+    {
+        get {return INCREASE_PER_GEOCHILD; }
     }
 }
