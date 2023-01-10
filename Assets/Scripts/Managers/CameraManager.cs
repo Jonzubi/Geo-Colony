8b6df3f9a7f15749e8fb02c925ad685575659b9f
Jon Zubizarreta <jonzubi97@gmail.com>
2023-01-10 22:22:42 +0100

    Movimiento funciona aunque es una mierda

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -9,15 +9,24 @@ public class CameraManager : MonoBehaviour
     [SerializeField] float zoomScale = 1f;
     [SerializeField] float moveScale = 1f;
     GameManager _gameManager;
-
+    Transform _cameraTransform;
     CinemachineComponentBase _componentBase;
 
+    bool isDragging = false;
+    Vector3 startDragging;
     void Awake()
     {
-        _gameManager = FindObjectOfType<GameManager>();    
+        _gameManager = FindObjectOfType<GameManager>();
+        _cameraTransform = _cinemachineVirtualCamera.transform;    
     }
 
     void Update()
+    {
+        ManageZoom();
+        ManageMovement();
+    }
+
+    void ManageZoom()
     {
         float mouseWheel = Input.mouseScrollDelta.y;
         if (mouseWheel != 0)
@@ -28,6 +37,30 @@ public class CameraManager : MonoBehaviour
             if (newSize < 5)
                 newSize = 5;            
             _cinemachineVirtualCamera.m_Lens.OrthographicSize = Mathf.SmoothStep(oldSize, newSize, 1f);
+        }
+    }
+
+    void ManageMovement()
+    {
+        if (!isDragging && Input.GetMouseButtonDown(0))
+        {
+            startDragging = Input.mousePosition;
+            isDragging = true;
+            return;
+        }      
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+            return;
+        }
+
+        if (isDragging)
+        {
+            Vector3 actualPosition = Input.mousePosition;
+            Vector3 direction = -(actualPosition - startDragging).normalized;
+            Debug.Log(direction);
+            _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, _cameraTransform.position + direction * moveScale, Time.deltaTime);
         }
     }
 }
